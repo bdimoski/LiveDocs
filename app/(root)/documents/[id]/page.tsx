324f8a5a02c364b96ee80cb0f3422fa22f0fd9ba
Bojan <bdimosky35@yahoo.com>
2024-08-01 09:07:45 +0200

    Add Liveblocks features, implement user authentication, and create homepage

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
@@ -5,20 +6,9 @@ import React from 'react'
 
 const Document = () => {
     return (
-        <div>
-            <Header>
-                <div className='flex w-fit items-center justify-center gap-2'>
-                    <p className='document-title'>Share</p>
-                </div>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </Header>
-            <Editor />
-        </div>
+        <main className='flex w-full flex-col items-center'>
+            <CollaborativeRoom />
+        </main>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils"
 import { Metadata } from "next"
 import { ClerkProvider } from "@clerk/nextjs"
 import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -34,7 +35,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>
+            {children}
+          </Provider>
         </body>
       </html>
     </ClerkProvider>
