3a12a0dd6355312e4d5f4144a9a1e3f3337d5120
Bojan <bdimosky35@yahoo.com>
2024-08-01 19:44:13 +0200

    Add getDocuments and getClerkUsers actions

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -72,3 +72,13 @@ export const updateDocument = async (roomId: string, title: string) => {
     console.log(`Error happened while updating the document: ${error}`);
   }
 };
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting the rooms: ${error}`);
+  }
+};
