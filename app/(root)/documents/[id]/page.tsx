0741cc99fd6506901e2b1e56e08f96c88a9e2da9
Bojan <bdimosky35@yahoo.com>
2024-08-01 20:36:17 +0200

    Add Liveblocks integration and active collaborators component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,43 @@
 import CollaborativeRoom from '@/components/CollaborativeRoom'
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { getDocument } from '@/lib/actions/room.actions'
+import { getClerkUsers } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+    const clerkUser = await currentUser()
+    if (!clerkUser) redirect('/sign-in')
+
+    const room = await getDocument({
+        roomId: id,
+        userId: clerkUser.emailAddresses[0].emailAddress
+    })
+
+    if (!room) redirect('/')
+
+    const userIds = Object.keys(room.usersAccesses)
+    const users = await getClerkUsers({ userIds })
+
+    const usersData = users.map((user: User) => ({
+        ...user,
+        userType: room.usersAccesses[user.email]?.includes('room:write')
+            ? 'editor'
+            : 'viewer'
+    }))
+
+    const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes
+        ('room:write') ? 'editor' : 'viewer'
+
     return (
         <main className='flex w-full flex-col items-center'>
-            <CollaborativeRoom />
+            <CollaborativeRoom
+                roomId={id}
+                roomMetadata={room.metadata}
+                users={usersData}
+                currentUserType={currentUserType}
+            />
+
         </main>
     )
 }
