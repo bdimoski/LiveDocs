de0e654e117fde7c62ebd6a5ca45211053796e5d
Bojan <bdimosky35@yahoo.com>
2024-08-02 11:41:05 +0200

    Add share document functionality and user permissions

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,22 @@
-import CollaborativeRoom from '@/components/CollaborativeRoom'
-import { getDocument } from '@/lib/actions/room.actions'
-import { getClerkUsers } from '@/lib/actions/user.actions'
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import CollaborativeRoom from "@/components/CollaborativeRoom"
+import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
-    const clerkUser = await currentUser()
-    if (!clerkUser) redirect('/sign-in')
+    const clerkUser = await currentUser();
+    if (!clerkUser) redirect('/sign-in');
 
     const room = await getDocument({
         roomId: id,
-        userId: clerkUser.emailAddresses[0].emailAddress
-    })
+        userId: clerkUser.emailAddresses[0].emailAddress,
+    });
 
-    if (!room) redirect('/')
+    if (!room) redirect('/');
 
-    const userIds = Object.keys(room.usersAccesses)
-    const users = await getClerkUsers({ userIds })
+    const userIds = Object.keys(room.usersAccesses);
+    const users = await getClerkUsers({ userIds });
 
     const usersData = users.map((user: User) => ({
         ...user,
@@ -26,18 +25,16 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
             : 'viewer'
     }))
 
-    const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes
-        ('room:write') ? 'editor' : 'viewer'
+    const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
 
     return (
-        <main className='flex w-full flex-col items-center'>
+        <main className="flex w-full flex-col items-center">
             <CollaborativeRoom
                 roomId={id}
                 roomMetadata={room.metadata}
                 users={usersData}
                 currentUserType={currentUserType}
             />
-
         </main>
     )
 }
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from "./ui/input"
 import Image from "next/image"
 import { updateDocument } from "@/lib/actions/room.actions"
 import Loader from "./Loader"
+import ShareModal from "./ShareModal"
 
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
     const [editing, setEditing] = useState(false)
@@ -100,6 +101,12 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
                         </div>
                         <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                             <ActiveCollaborators />
+                            <ShareModal
+                                roomId={roomId}
+                                collaborators={users}
+                                creatorId={roomMetadata.creatorId}
+                                currentUserType={currentUserType}
+                            />
                             <SignedOut>
                                 <SignInButton />
                             </SignedOut>
